532fd16b274b4cf0b912ad76f5e5d54bc5cbd5af
zhangyu_0427 <1802833394@qq.com>
2021-07-22 00:59:02 +0800

    第八天

---
+++ day08/src/org/Zzzzzz/exer/PersonTest.java
@@ -0,0 +1,17 @@
+package org.Zzzzzz.exer;
+
+public class PersonTest {
+	public static void main(String[] args) {
+		Person p1 = new Person();
+		
+		p1.name = "Tom";
+		p1.age = 18;
+		p1.sex = 1;
+		
+		p1.study();
+		p1.showage();
+		int newAge = p1.addAge(2);
+		
+		System.out.println(newAge);
+	}
+}
