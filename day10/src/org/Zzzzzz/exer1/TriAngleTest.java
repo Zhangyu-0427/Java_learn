3168d728e9c86833c315d6300060c2c84d0d9078
zhangyu_0427 <1802833394@qq.com>
2021-07-31 23:50:23 +0800

    第十天

---
+++ day10/src/org/Zzzzzz/exer1/TriAngleTest.java
@@ -0,0 +1,13 @@
+package org.Zzzzzz.exer1;
+
+public class TriAngleTest {
+
+	public static void main(String[] args) {
+		
+		TriAngle test = new TriAngle(2.3, 4.00);
+		
+		System.out.println("底：" + test.getBase() + ",高：" + test.getHeight());
+		
+	}
+	
+}
