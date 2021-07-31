fabaf36701f668d9c9df69337908004bfdb01c53
zhangyu_0427 <1802833394@qq.com>
2021-07-31 23:49:32 +0800

    第十天

---
+++ day10/src/org/Zzzzzz/exer1/TriAngle.java
@@ -0,0 +1,33 @@
+package org.Zzzzzz.exer1;
+
+public class TriAngle {
+
+	private double base;
+	private double height;
+	
+	public TriAngle(){
+		
+	}
+	
+	public TriAngle(double b, double h){
+		base = b;
+		height = h;
+	}
+	
+	
+	public void setBase(double a){
+		base = a;
+	}
+	
+	public double getBase(){
+		return base;
+	}
+	
+	public void setHeight(double a){
+		height = a;
+	}
+	
+	public double getHeight(){
+		return height;
+	}
+}
