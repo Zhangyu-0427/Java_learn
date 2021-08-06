b28e7ef2756253d6dcd3dceeeb4f5d85a6fa4bb1
zhangyu_0427 <1802833394@qq.com>
2021-08-06 22:47:59 +0800

    第十天

---
+++ day10/src/org/Zzzzzz/exer4/BankTest.java
@@ -0,0 +1,26 @@
+package org.Zzzzzz.exer4;
+
+public class BankTest {
+
+	public static void main(String[] args) {
+		
+		Bank bank = new Bank();
+		bank.addCustomer("Jane", "Smith");
+		
+		bank.getCustomer(0).setAccount(new Account(2000));
+		
+		bank.getCustomer(0).getAccount().withdraw(500);
+		
+		double balance = bank.getCustomer(0).getAccount().getBalance();
+		
+		System.out.println("客户：" +bank.getCustomer(0).getFirstName() + "当前余额为：" + balance);
+		
+		System.out.println("**************************");
+		bank.addCustomer("Zz", "Gg");
+		
+		System.out.println("银行客户数量为：" + bank.getNumOfCustomers());
+		
+		
+	}
+	
+}
